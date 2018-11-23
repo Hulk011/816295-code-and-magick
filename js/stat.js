f4081450ead4433f9481873ddaa0458fcd9d792a
Hulk011 <panther7777771@gmail.com>
2018-11-23 23:10:31 +0700

    Улучшение читаемости кода в отрисовке гистораммы

--- js/stat.js
+++ js/stat.js
@@ -6,11 +6,9 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_HEIGTH = 270;
 var CLOUD_WIDTH = 420;
-
 var SHADOW_OFFSET = 10;
 var SHADOW_X = CLOUD_X + SHADOW_OFFSET;
 var SHADOW_Y = CLOUD_Y + SHADOW_OFFSET;
-
 var TEXT_FONT = '16px PT Mono';
 var TEXT_COLOR = 'rgba(0, 0, 0, 1)';
 var MESSAGE_CONGRATULATIONS = 'Ура, вы победили!';
@@ -18,14 +16,15 @@ var MESSAGE_X = 120;
 var MESSAGE_Y = 40;
 var MESSAGE_RESULT = 'Список результатов';
 var MESSAGE_OFFSET = 20;
-
-var HISTOGRAM_HEIGHT = 150;
+var HISTOGRAM_HEIGHT_MAX = 150;
+var HISTOGRAM_OFFSET_X = 60;
 var STEP_BETWEEN_COLUMN = 30;
 var COLUMN_WIDTH = 40;
 var COLUMN_COLOR_YOU = 'rgba(255, 0, 0, 1)';
-
 var MAIN_PLAYER_NAME = 'Вы';
-
+var BAR_NAME_Y = CLOUD_Y + CLOUD_HEIGTH - STEP_BETWEEN_COLUMN / 2;
+var BAR_OFFSET_Y = 80;
+var TIME_OFFSET_Y = 60;
 
 var getRandom = function (min, max) {
   return Math.random() * (max - min) + min;
@@ -63,25 +62,31 @@ var getMaxTime = function (times) {
   return maxTimes;
 };
 
-var renderGistagramm = function (ctx, names, times) {
+var renderHistagramm = function (ctx, names, times) {
   var maxTime = getMaxTime(times);
-  var timeWhole;
+  var time;
   var height;
+  var heightDifference;
+  var barY;
+  var timeY;
   var x;
-  var y;
 
   for (var i = 0; i < names.length; i++) {
-    timeWhole = Math.round(times[i]);
-    height = HISTOGRAM_HEIGHT * times[i] / maxTime;
-    x = CLOUD_X + STEP_BETWEEN_COLUMN * 2 + (COLUMN_WIDTH + STEP_BETWEEN_COLUMN * 1.5) * i;
-    y = CLOUD_Y + STEP_BETWEEN_COLUMN * 2 + (HISTOGRAM_HEIGHT - height);
+    time = Math.round(times[i]);
+    height = times[i] / maxTime * HISTOGRAM_HEIGHT_MAX;
+    heightDifference = HISTOGRAM_HEIGHT_MAX - height;
 
-    ctx.fillStyle = (names[i] === MAIN_PLAYER_NAME) ? COLUMN_COLOR_YOU : generateBarColor();
-    ctx.fillRect(x, y + STEP_BETWEEN_COLUMN, COLUMN_WIDTH, height);
+    barY = CLOUD_Y + heightDifference + BAR_OFFSET_Y;
+    timeY = CLOUD_Y + heightDifference + TIME_OFFSET_Y;
+
+    x = CLOUD_X + HISTOGRAM_OFFSET_X + i * (COLUMN_WIDTH + STEP_BETWEEN_COLUMN);
+
+    ctx.fillStyle = names[i] === MAIN_PLAYER_NAME ? COLUMN_COLOR_YOU : generateBarColor();
+    ctx.fillRect(x, barY, COLUMN_WIDTH, height);
 
     ctx.fillStyle = TEXT_COLOR;
-    ctx.fillText(names[i], x, CLOUD_Y + CLOUD_HEIGTH - (STEP_BETWEEN_COLUMN / 2));
-    ctx.fillText(timeWhole, x, y + (STEP_BETWEEN_COLUMN / 1.75));
+    ctx.fillText(names[i], x, BAR_NAME_Y);
+    ctx.fillText(time, x, timeY);
   }
 };
 
@@ -89,5 +94,5 @@ window.renderStatistics = function (ctx, names, times) {
   renderShadow(ctx);
   renderCloud(ctx);
   renderMessages(ctx);
-  renderGistagramm(ctx, names, times);
+  renderHistagramm(ctx, names, times);
 };
